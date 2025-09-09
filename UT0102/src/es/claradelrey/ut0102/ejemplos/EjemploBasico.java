3dfe873a195bc7b16c2d2b0298c6341172e4c4c1
maortiz17 <maos17@educa.madrid.org>
2025-09-09 07:05:55 +0200

    modificación básica

--- UT0102/src/es/claradelrey/ut0102/ejemplos/EjemploBasico.java
+++ UT0102/src/es/claradelrey/ut0102/ejemplos/EjemploBasico.java
@@ -3,7 +3,7 @@ package es.claradelrey.ut0102.ejemplos;
 public class EjemploBasico {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo!!");
+		System.out.println("Hola mundo cruel!!");
 	}
 
 }
